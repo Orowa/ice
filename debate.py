e35f7fa88377f734103531be3c4cf322d94b4754
orowa <orowa@thenetworklab.co>
2022-10-12 01:26:53 +0100

    Implements debate judge

--- debate.py
+++ debate.py
@@ -2,6 +2,19 @@ from ice.agents.base import Agent
 from ice.recipe import recipe
 from ice.recipes.primer.debate.prompt import *
 
+def judge_debate_prompt(debate_history: Debate) -> Name:
+    rendered_debate = render_debate(debate_history)
+    prompt = f"""
+    You are judging the debate. You are trying to decide who won the debate using reason and evidence. Return the name of the victor, and explain why they won.
+    
+    *Debate*
+    {rendered_debate}
+
+    *Judgment*
+    The name of the victor is: "
+    """.strip()
+    return prompt
+
 async def debate(question: str = "Should we ban guns?", number_of_turns: int = 8, agent_names: tuple[Name, Name] = ("Alice", "Bob")):
     agents = [recipe.agent(), recipe.agent()]
     debate_history = initialize_debate(question, agent_names)
@@ -12,6 +25,8 @@ async def debate(question: str = "Should we ban guns?", number_of_turns: int = 8
             answer = await agent.answer(prompt=prompt, multiline=False)
             debate_history.append((name, answer.strip('" ')))
             turns_left -= 1
-    return render_debate(debate_history)
+    judgment_prompt = judge_debate_prompt(debate_history)
+    victor = await recipe.agent().answer(prompt=judgment_prompt)
+    return judgment_prompt + victor
 
 recipe.main(debate)
