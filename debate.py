472825628afef0eb55348c886783fecbea5d8799
orowa <orowa@thenetworklab.co>
2022-10-13 00:16:44 +0100

    Scaffolding for the multi-response debator

--- debate.py
+++ debate.py
@@ -1,3 +1,4 @@
+import itertools
 from ice.agents.base import Agent
 from ice.recipe import recipe
 from ice.recipes.primer.debate.prompt import *
@@ -19,10 +20,15 @@ async def debate(question: str = "Should we ban guns?", number_of_turns: int = 8
     agents = [recipe.agent(agent_name="instruct-stochastic"), recipe.agent(agent_name="instruct-stochastic")]
     debate_history = initialize_debate(question, agent_names)
     turns_left = number_of_turns
+    cache_id = itertools.count()
     while turns_left > 0:
         for agent, name in zip(agents, agent_names):
             prompt = render_debate_prompt(name, debate_history, turns_left)
-            answer = await agent.complete(prompt=prompt, stop="\n")
+            potential_answers = []
+            for _ in range(3):
+                answer = await agent.complete(prompt=prompt, stop="\n", cache_id=next(cache_id))
+                potential_answers.append(answer)
+            # TODO: Use the best answer as judged by the one with the highest win probability by the judge.
             debate_history.append((name, answer.strip('" ')))
             turns_left -= 1
     judgment_prompt = judge_debate_prompt(debate_history)
